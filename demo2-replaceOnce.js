8ee60c63569703a2209ef78c0e3e9701eb74f9b3
Kaige Ye <ye@kaige.org>
2019-03-12 11:46:08 +0800

    fix bug

--- demo2-replaceOnce.js
+++ demo2-replaceOnce.js
@@ -1,90 +1,103 @@
-let replacedFlag = false;
-
-function isInTagAttribute(text, index) {
-    const lastIndexOfLeftArrow = text.lastIndexOf('<', index);
-    const lastIndexOfRightArrow = text.lastIndexOf('>', index);
-
-    const nextIndexOfLeftArrow = text.indexOf('<', index);
-    const nextIndexOfRightArrow = text.indexOf('>', index);
-
-    if (lastIndexOfLeftArrow === -1 || nextIndexOfRightArrow === -1 ||
-        lastIndexOfRightArrow > lastIndexOfLeftArrow || nextIndexOfLeftArrow < nextIndexOfRightArrow) {
-        return false;
-    }
-    return true;
-}
-
 function isSpaceCharacter(ch) {
     return ch === '\x20' || ch === '\x09' || ch === '\x0A' || ch === '\x0C' || ch === '\x0D';
 }
 
 function isTagAStart(text, i) {
-    const charAfterTag = text.charAt(i + 2);
-    return (text.startsWith('<a', i) || text.startsWith('<A', i)) && (isSpaceCharacter(charAfterTag) || charAfterTag === '>');
+    const TAG_A_START = '<a';
+    const charAfterTag = text.charAt(i + TAG_A_START.length);
+    return (text.startsWith(TAG_A_START, i) || text.startsWith(TAG_A_START.toUpperCase(), i)) &&
+        (isSpaceCharacter(charAfterTag) || charAfterTag === '>');
 }
 
 function isTagAEnd(text, i) {
-    const charAfterTag = text.charAt(i + 3);
-    return (text.startsWith('</a', i) || text.startsWith('</A', i)) && (isSpaceCharacter(charAfterTag) || charAfterTag === '>');
+    const TAG_A_END = '</a';
+    const charAfterTag = text.charAt(i + TAG_A_END.length);
+    return (text.startsWith(TAG_A_END, i) || text.startsWith(TAG_A_END.toUpperCase(), i)) &&
+        (charAfterTag === '>' || isSpaceCharacter(charAfterTag));
+}
+
+function isInTagAttribute(text, index) {
+    const lastIndexOfLT = text.lastIndexOf('<', index);
+    const lastIndexOfGT = text.lastIndexOf('>', index);
+    const nextIndexOfLT = text.indexOf('<', index);
+    const nextIndexOfGT = text.indexOf('>', index);
+
+    return !(lastIndexOfLT === -1 || nextIndexOfGT === -1 ||
+        lastIndexOfLT < lastIndexOfGT  || nextIndexOfLT < nextIndexOfGT);
 }
 
 function isInTagA(text, index) {
-    const lastIndexOfLeftArrow = text.lastIndexOf('<', index);
-    const lastIndexOfRightArrow = text.lastIndexOf('>', index);
+    const lastIndexOfLT = text.lastIndexOf('<', index);
+    const lastIndexOfGT = text.lastIndexOf('>', index);
+    const nextIndexOfLT = text.indexOf('<', index);
+    const nextIndexOfGT = text.indexOf('>', index);
 
-    const nextIndexOfLeftArrow = text.indexOf('<', index);
-    const nextIndexOfRightArrow = text.indexOf('>', index);
-
-    if (lastIndexOfLeftArrow >= 0 &&
-        lastIndexOfLeftArrow < lastIndexOfRightArrow && 
-        lastIndexOfRightArrow < nextIndexOfLeftArrow &&
-        nextIndexOfLeftArrow < nextIndexOfRightArrow &&
-        isTagAStart(text, lastIndexOfLeftArrow) &&
-        isTagAEnd(text, nextIndexOfLeftArrow)
-        ) {
-        return true;
-    }
-    return false;
+    return 0 <= lastIndexOfLT &&
+        lastIndexOfLT < lastIndexOfGT &&
+        lastIndexOfGT < nextIndexOfLT &&
+        nextIndexOfLT < nextIndexOfGT &&
+        isTagAStart(text, lastIndexOfLT) &&
+        isTagAEnd(text, nextIndexOfLT);
 }
 
-function replaceAt(text, find, replacement, index) {
-    let s = text;
-    let flag = isInTagAttribute(text, index);
-    let flag2 = isInTagA(text, index);
-    if (!flag && !flag2) {
-        s = text.substring(0, index) + replacement + text.substring(index + replacement.length);
-        replacedFlag = true;
-    }
-    return s;
-}
-
-function replace_alg2(text, find, replacement) {
+function replaceFirst_alg2(text, find, replacement) {
     let fromIndex = 0;
     let i = -1;
-    while((i = text.indexOf(find, fromIndex)) !== -1 && !replacedFlag) {
-        // console.log(i, fromIndex);
-        text = replaceAt(text, find, replacement, i);
-        fromIndex = i + replacement.length;
+    while((i = text.indexOf(find, fromIndex)) !== -1) {
+        if (isInTagAttribute(text, i) || isInTagA(text, i)) {
+            fromIndex = i + find.length;
+        } else {
+            text = text.substring(0, i) + replacement + text.substring(i + find.length);
+            break; // fromIndex = i + replacement.length; // replaceAll
+        }
     }
     return text;
 }
 
-const { performance } = require('perf_hooks');
-
-function run(text, find, replacement, alg) {
-    let sum = 0;
-    const n = 20;
-    for (let t = 0; t < n; t++) {
-        let t1 = performance.now();
-        for (let i = 0; i < 10000; i++) {
-            alg(text, find, replacement);
-        }
-        let t2 = performance.now();
-        sum += t2 - t1;
-        console.log(alg.name, t2 - t1);
-    }
-    console.log('avg', sum/n);
-}
-
 let s = '票房票房html<a>票房</a>票房AA\x01BB<a>wng票房e1kk</a> 票房票房loong票房票房 <a href="票房" alter="nXnwgX9">X票房票房X</a>end票房wng213<p e>票房</p><ax票房>票房</ax><img src="票房"/><a >票房</a>票房票房';
-run(s, '票房', '十一', replace_alg2);
+let r = replaceFirst_alg2(s, '票房', '十一');
+console.log(r);
+console.log(replaceFirst_alg2('<a>票房</a>票房123456789012345678901234567890票房', '票房', '<a class=“matched”>票房</a >'));
+console.log(replaceFirst_alg2('<p span="票房">票房</p>', '票房', '<a class=“matched”>票房</a >'));
+
+
+// function replaceAll_alg2(text, find, replacement) {
+//     let fromIndex = 0;
+//     let i = -1;
+//     while((i = text.indexOf(find, fromIndex)) !== -1) {
+//         if (isInTagAttribute(text, i) || isInTagA(text, i)) {
+//             fromIndex = i + find.length;
+//         } else {
+//             text = text.substring(0, i) + replacement + text.substring(i + find.length);
+//             fromIndex = i + replacement.length;
+//         }
+//     }
+//     return text;
+// }
+
+
+//
+// let replacedFlag;
+//
+// function replaceAt(text, find, replacement, index) {
+//     let s = text;
+//     if (!isInTagAttribute(text, index) && !isInTagA(text, index)) {
+//         s = text.substring(0, index) + replacement + text.substring(index + replacement.length);
+//         replacedFlag = true;
+//     }
+//     return s;
+// }
+//
+// function replace_alg2(text, find, replacement) {
+//     replacedFlag = false;
+//     let fromIndex = 0;
+//     let i = -1;
+//     while((i = text.indexOf(find, fromIndex)) !== -1 && !replacedFlag) {
+//         // console.log(i, fromIndex);
+//         text = replaceAt(text, find, replacement, i);
+//         fromIndex = i + replacement.length;
+//     }
+//     return text;
+// }
+
+
