c8f99065150d729592d30403fec21895b15f85db
Kaige Ye <ye@kaige.org>
2019-03-20 17:59:33 +0800

    refactor and add tests

--- z.js
+++ z.js
@@ -1,19 +1,22 @@
+function lastIndexOfIgnoreCase(text, lowerCasePrefix, fromIndex) {
+    let start = text.lastIndexOf(lowerCasePrefix, fromIndex);
+    return start === -1 ? text.lastIndexOf(lowerCasePrefix.toUpperCase(), fromIndex) : start;
+}
+
+function indexOfIgnoreCase(text, lowerCasePrefix, fromIndex) {
+    let start = text.indexOf(lowerCasePrefix, fromIndex);
+    return start === -1 ? text.indexOf(lowerCasePrefix.toUpperCase(), fromIndex) : start;
+}
+
 function getLastTagAStart(text, fromIndex) {
     const re = /^<a[\s>]/i;
+    const PREFIX = '<a';
 
-    let start = -1;
-    while(fromIndex >= 0) {
-        start = text.lastIndexOf('<a', fromIndex);
-        start = start === -1 ? text.lastIndexOf('<A', fromIndex) : start;
-        if (start === -1) break;
-
+    let start;
+    while(fromIndex >= 0 && ((start = lastIndexOfIgnoreCase(text, PREFIX, fromIndex)) !== -1)) {
         const str = text.slice(start, start + 3);
-        if (re.test(str)) {
-            return start;
-        } else {
-            fromIndex = start - 1;
-        }
-        //console.log('---', fromIndex);
+        if (re.test(str)) return start;
+        fromIndex = start - 1;
     }
 
     return -1;
@@ -21,20 +24,13 @@ function getLastTagAStart(text, fromIndex) {
 
 function getFirstTagAEnd(text, fromIndex) {
     const re = /^<\/a[>\s]/i;
+    const PREFIX = '</a';
 
-    let start = -1;
-    while(fromIndex < text.length) {
-        start = text.indexOf('</a', fromIndex);
-        start = start === -1 ? text.indexOf('</A', fromIndex) : start;
-        if (start === -1) break;
-
+    let start;
+    while(fromIndex < text.length && ((start = indexOfIgnoreCase(text, PREFIX, fromIndex)) !== -1)) {
         const str = text.slice(start, start + 4);
-        if (re.test(str)) {
-            return start;
-        } else {
-            fromIndex = start + 3;
-        }
-        //console.log('---+++', fromIndex);
+        if (re.test(str)) return start;
+        fromIndex = start + 3;
     }
 
     return -1;
@@ -42,11 +38,9 @@ function getFirstTagAEnd(text, fromIndex) {
 
 const isInTagA = function (text, index) {
     const start = getLastTagAStart(text, index);
-    // console.log('a', start);
     if (start === -1) return false;
     const end = getFirstTagAEnd(text, start);
-    // console.log('e', end);
-    if (end === -1) return false;
+    if (end === -1) return false; // TBD
 
     return index < end;
 };
@@ -57,7 +51,7 @@ const isInTagAttribute = function (text, index) {
     const start = text.lastIndexOf('<', index);
     if (start === -1) return false;
     const end = text.indexOf('>', index);
-    if (end === -1) return false;
+    if (end === -1) return false; // TBD
 
     return pattern.test(text.substring(start, end + 1));
 };
@@ -78,6 +72,7 @@ function replaceFirst(text, find, replacement) {
 
 // ---- test ----
 
+console.log('abc'.slice(2, 5));
 
 let find = '票房';
 let replacement = '十一';
@@ -89,11 +84,11 @@ function run(text) {
 
 }
 
+run('<App href="#"> <span>票房</span> </App> 票房');
+run('<app href="#"> <span>票房</span> </app> 票房');
+run('<A href="#"> <span>票房</span> </A> 票房');
 run('<a href="#"> <span>票房</span> </a> 票房');
 run('<a href="#"> <div class="票房">票房</div> </a> 票房');
 run('<a href="#">x</a><span>票房</span> <a>y</a>');
 run('<a href="#"> <audio>票房</audio> </a> 票房');
 run('<a href="#"> <article>票房</article> <audio>票房</audio> </a> 票房');
-
-
-
